491103d389ebadc6cd1b4d59648b0810a79acb9f
Erica <elastufka@gmail.com>
2020-11-08 11:18:33 +0100

    take2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
-import plotly.express as px
+#import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
 import numpy as np
